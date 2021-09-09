8c7ca179dcb783666ea5b8f7c7e2ffdbc087af31
Artur <info@aigroupusa.com>
2021-09-09 14:31:29 -0400

    commit tree 03c18697f133568fe76a6b63619ef5a005c6f6f0

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,9 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.integer :taxi_id
+      t.integer :passenger_id
+      t.datetime :ride_date
     end
   end
 end
